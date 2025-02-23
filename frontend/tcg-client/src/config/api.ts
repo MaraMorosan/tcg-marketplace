8928b512003aff3fd5ed6ecf1200583056b38ce2
MaraMorosan <maramorosan95@gmail.com>
2025-02-23 13:37:18 +0200

    Created api file and V2 of the database through flyway

---
+++ frontend/tcg-client/src/config/api.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = `${process.env.NEXT_PUBLIC_BACKEND_URL}`;
+export const PRODUCTS_ENDPOINT = `${BASE_URL}/api/products/search`;
+export const CARDS_ENDPOINT = `${BASE_URL}/api/cards`
